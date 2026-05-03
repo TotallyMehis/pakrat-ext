1b7ea121853a7628923230358559f14b4bb32f94
TotallyMehis <miikka@mehis.dev>
2026-05-03 21:28:36 +0300

    Refactor FileTransferHandler

--- src/main/java/pak/FileTransferHandler.java
+++ src/main/java/pak/FileTransferHandler.java
@@ -10,48 +10,47 @@ import javax.swing.JComponent;
 import javax.swing.TransferHandler;
 
 class FileTransferHandler extends TransferHandler {
-   private DataFlavor fileFlavor;
-   private Unpak pakrat;
+   private static final DataFlavor FILE_FLAVOR = DataFlavor.javaFileListFlavor;
+   private final Unpak pakrat;
 
    public FileTransferHandler(Unpak pakrat) {
       this.pakrat = pakrat;
-      this.fileFlavor = DataFlavor.javaFileListFlavor;
    }
 
    @Override
    public boolean importData(JComponent c, Transferable t) {
       if (!this.canImport(c, t.getTransferDataFlavors())) {
          return false;
-      } else {
-         try {
-            @SuppressWarnings("unchecked")
-            List<File> files = (List<File>) t.getTransferData(this.fileFlavor);
-            File[] filearray = (File[]) files.toArray(new File[0]);
-            this.pakrat.addfiletopak(filearray, this.pakrat.gamedir, false);
-            return true;
-         } catch (UnsupportedFlavorException var5) {
-            Cons.println("importData: unsupported data flavor");
-         } catch (IOException var6) {
-            Cons.println("importData: I/O exception");
-         }
-
-         return false;
       }
+
+      try {
+         @SuppressWarnings("unchecked")
+         List<File> files = (List<File>) t.getTransferData(FILE_FLAVOR);
+         File[] filearray = (File[]) files.toArray(new File[0]);
+         this.pakrat.addfiletopak(filearray, this.pakrat.gamedir, false);
+         return true;
+      } catch (UnsupportedFlavorException var5) {
+         Cons.println("importData: unsupported data flavor");
+      } catch (IOException var6) {
+         Cons.println("importData: I/O exception");
+      }
+
+      return false;
    }
 
    @Override
    public int getSourceActions(JComponent c) {
-      return 1;
+      return TransferHandler.COPY;
    }
 
    @Override
    public boolean canImport(JComponent c, DataFlavor[] flavors) {
-      return this.hasFileFlavor(flavors);
+      return hasFileFlavor(flavors);
    }
 
-   private boolean hasFileFlavor(DataFlavor[] flavors) {
-      for (int i = 0; i < flavors.length; ++i) {
-         if (this.fileFlavor.equals(flavors[i])) {
+   private static boolean hasFileFlavor(DataFlavor[] flavors) {
+      for (DataFlavor flavor : flavors) {
+         if (FILE_FLAVOR.equals(flavor)) {
             return true;
          }
       }
