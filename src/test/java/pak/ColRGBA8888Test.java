b2c9be9909590c24b393dfe6786d346205b2e164
TotallyMehis <miikka@mehis.dev>
2026-05-05 21:36:20 +0300

    More formatting changes

--- src/test/java/pak/ColRGBA8888Test.java
+++ src/test/java/pak/ColRGBA8888Test.java
@@ -6,13 +6,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 final class ColRGBA8888Test {
-    @CsvSource(
-        """
-        255,255,255,255,255,-1,65535
-        128,128,132,16,0,-1,32896
-        200,5,0,186,66,-1,1480
-        32,0,0,4,0,-1,32
-        """)
+    @CsvSource("""
+            255,255,255,255,255,-1,65535
+            128,128,132,16,0,-1,32896
+            200,5,0,186,66,-1,1480
+            32,0,0,4,0,-1,32
+            """)
     @ParameterizedTest
     void color(int byte0, int byte1, int red, int green, int blue, int alpha, int c565) {
         ColRGBA8888 color = ColRGBA8888.from565(byte0, byte1);
--- src/test/java/pak/FileTypeTest.java
+++ src/test/java/pak/FileTypeTest.java
@@ -6,19 +6,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 final class FileTypeTest {
-    @CsvSource(
-        """
-        file.txt,TEXT
-        file.vmt,MATERIAL
-        file.vtf,TEXTURE
-        file.mdl,MODEL
-        file.vvd,MODEL_DAT
-        file.ani,MODEL_DAT
-        file.vtx,MODEL_DAT
-        file.mp3,SOUND
-        file.wav,SOUND
-        file.asdf,OTHER
-        """)
+    @CsvSource("""
+            file.txt,TEXT
+            file.vmt,MATERIAL
+            file.vtf,TEXTURE
+            file.mdl,MODEL
+            file.vvd,MODEL_DAT
+            file.ani,MODEL_DAT
+            file.vtx,MODEL_DAT
+            file.mp3,SOUND
+            file.wav,SOUND
+            file.asdf,OTHER
+            """)
     @ParameterizedTest
     void fromFileName(String fileName, FileType expected) {
         assertEquals(expected, FileType.from(fileName));
