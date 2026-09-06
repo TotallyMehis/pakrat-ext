aa33ef1ffe897db8c33dac7110e46f40858750f3
TotallyMehis <miikka@mehis.dev>
2026-09-06 10:20:46 +0300

    Error prone fix

--- src/main/java/pak/PakratUI.java
+++ src/main/java/pak/PakratUI.java
@@ -1,9 +1,7 @@
 package pak;
 
 public abstract class PakratUI {
-    private static final String PAKRAT_TITLE = "Pakrat %s - Original Pakrat 0.95 by Rof";
-
     public static String getPakratTitle() {
-        return PAKRAT_TITLE.formatted(Version.getFullVersion());
+        return "Pakrat %s - Original Pakrat 0.95 by Rof".formatted(Version.getFullVersion());
     }
 }
